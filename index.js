5a42de504afc7fde7f04d001de7ac30b01da24f3
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-16 23:06:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function prependKitten(name){    //prepends a kitten to the kittens array and re
 }
 
 function removeLastKitten(){ //removes the last kitten in the kittens; array unchanged
-   kittens.pop()
-   return kittens
+   return kittens.pop()
+   
   
 }
 
